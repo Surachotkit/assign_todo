9cb60ac05519ec6e915b411480acd59a42eec642
Surachot Kittivijarn <surachot.kit@gmail.com>
2025-03-14 01:35:39 +0700

    init commit

---
+++ src/pages/List.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function List() {
+  return (
+    <>
+        <div className='flex justify-center'>List</div>
+        <h1 className='border-amber-500 flex bg-amber-500'>List</h1>
+    </>
+  )
+}
